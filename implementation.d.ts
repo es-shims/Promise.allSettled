14d080aa97b4ac66d0f22a5d58d0a7fe3c6a0a47
Martin Jurča <mjurca@centrum.cz>
2019-04-25 09:03:37 +0200

    fixed code style

--- implementation.d.ts
+++ implementation.d.ts
@@ -10,7 +10,7 @@ interface PromiseRejection<E> {
 
 type PromiseResult<T, E> = PromiseResolution<T> | PromiseRejection<E>;
 type PromiseTuple<T extends [any, ...any[]]> = {[P in keyof T]: Promise<T[P]>};
-type PromiseResultTuple<T extends [any, ...any[]]> = {[P in keyof T]: PromiseResult<T[P], any>}
+type PromiseResultTuple<T extends [any, ...any[]]> = {[P in keyof T]: PromiseResult<T[P], any>};
 
 declare function allSettled(): Promise<[]>
 declare function allSettled<T extends [any, ...any[]]>(iterable: PromiseTuple<T>): Promise<PromiseResultTuple<T>>
